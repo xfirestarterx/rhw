8b87677ed9464cd00f9931bca0a3f0a9a9665e8f
blah <blah@blah.blah>
2020-11-19 20:15:11 +0200

    cards content

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,24 +2,55 @@ import React from 'react';
 import './Card.css';
 import Button from "../Button/Button";
 
-const Card = ({user, deleteHandler}) => {
-    // console.log(idx)
-
+const CardRecord = ({ user }) => {
     const {
-        id,
         name,
-        username,
         email,
-        address,
+        username,
         phone,
-        website,
-        company
+        website
     } = user;
 
     return (
+      <div className="CardRecord">
+          <div><i>Name:</i> {name}</div>
+          <div><i>Email:</i> {email}</div>
+          <div><i>Username:</i> {username}</div>
+          <div><i>Phone:</i> {phone}</div>
+          <div><i>Website:</i> {website}</div>
+      </div>
+    );
+};
+
+const CardFooter = ({ user: { address, company } }) => {
+    const { street, suite, city, zipcode } = address;
+    const { name, catchPhrase, bs } = company;
+
+    return (
+        <div className="CardFooter">
+            <div className="CardFooterContainer">
+                <div className="CardFooterHeading">Address</div>
+                <span><i>Street:</i> {street}</span>
+                <span><i>Suite:</i> {suite}</span>
+                <span><i>City:</i> {city}</span>
+                <span><i>Zip code:</i> {zipcode}</span>
+            </div>
+            <div className="CardFooterContainer">
+                <div className="CardFooterHeading">Company</div>
+                <span><i>Name:</i> {name}</span>
+                <span><i>Catch phrase:</i> {catchPhrase}</span>
+                <span><i>BS:</i> {bs}</span>
+            </div>
+        </div>
+    );
+};
+
+const Card = ({user, deleteHandler}) => {
+    return (
         <div className="Card">
-            { id }
-            <Button clickHandler={() => deleteHandler(id)}>Delete</Button>
+            <CardRecord user={user} />
+            <CardFooter user={user} />
+            <Button clickHandler={() => deleteHandler(user.id)}>Delete</Button>
         </div>
     );
 };
