568e8f4612190b0d4caf3f0c408663c7eecd74f3
blah <blah@blah.blah>
2021-01-21 09:53:27 +0200

    added default map view

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 
-const Map = () => {
+const Map = withScriptjs(withGoogleMap(() => {
     return (
-        <div>map</div>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        ></GoogleMap>
     );
-}
+}));
 
 export default Map;
--- src/components/MapWrapper/MapWrapper.js
+++ src/components/MapWrapper/MapWrapper.js
@@ -4,7 +4,12 @@ import Map from '../Map/Map';
 const MapWrapper = () => {
     return (
         <div className="MapWrapper Wrapper">
-            <Map />
+            <Map
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div className="Map" />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
         </div>
     );
 }
